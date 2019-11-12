3553f74f33b18d8613217dc69e4e95a5917789e4
PetarGajevic <gajevicp0@gmail.com>
2019-11-12 14:52:33 +0100

    Modalna

--- js/main.js
+++ js/main.js
@@ -45,11 +45,92 @@ $(document).ready(function(){
         document.getElementById('htmls').style.overflow = "auto";
       }
       
-      // When the user clicks anywhere outside of the modal, close it
-      window.onclick = function(event) {
+      // When the user clicks anywhere outside of the modal, close it 
+      window.addEventListener("click",  function(event) {
         if (event.target == modal) {
           modal.style.display = "none";
+
+        }
+        document.getElementById('htmls').style.overflow = "auto";
+      })
+      // Modal 1
+      var modal1 = document.getElementById("myModal1");
+      var btn1 = document.getElementById("btn1");
+      var span1 = document.getElementsByClassName("close1")[0];
+      btn1.onclick = function() {
+        modal1.style.display = "block";
+        document.getElementById('htmls').style.overflow = "hidden";
+
+      }
+      span1.onclick = function() {
+        modal1.style.display = "none";
+        document.getElementById('htmls').style.overflow = "auto";
+      }
+      window.addEventListener("click",  function(event) {
+        if (event.target == modal1) {
+          modal1.style.display = "none";
           document.getElementById('htmls').style.overflow = "auto";
         }
-      }
+      })
+
+        // Modal 2
+        var modal2 = document.getElementById("myModal2");
+        var btn2 = document.getElementById("btn2");
+        var span2 = document.getElementsByClassName("close2")[0];
+        btn2.onclick = function() {
+          modal2.style.display = "block";
+          document.getElementById('htmls').style.overflow = "hidden";
+ 
+        }
+        span2.onclick = function() {
+          modal2.style.display = "none";
+          document.getElementById('htmls').style.overflow = "auto";
+        }
+        window.addEventListener("click",  function(event) {
+          if (event.target == modal2) {
+            modal2.style.display = "none";
+            document.getElementById('htmls').style.overflow = "auto";
+          }
+        })
+
+         // Modal 3
+         var modal3 = document.getElementById("myModal3");
+         var btn3 = document.getElementById("btn3");
+         var span3 = document.getElementsByClassName("close3")[0];
+         btn3.onclick = function() {
+           modal3.style.display = "block";
+           document.getElementById('htmls').style.overflow = "hidden";
+  
+         }
+         span3.onclick = function() {
+           modal3.style.display = "none";
+           document.getElementById('htmls').style.overflow = "auto";
+         }
+         window.addEventListener("click",  function(event) {
+           if (event.target == modal3) {
+             modal3.style.display = "none";
+             document.getElementById('htmls').style.overflow = "auto";
+           }
+         })
+
+         // Modal 4
+         var modal4 = document.getElementById("myModal4");
+         var btn4 = document.getElementById("btn4");
+         var span4 = document.getElementsByClassName("close4")[0];
+         btn4.onclick = function() {
+           modal4.style.display = "block";
+           document.getElementById('htmls').style.overflow = "hidden";
+  
+         }
+         span4.onclick = function() {
+           modal4.style.display = "none";
+           document.getElementById('htmls').style.overflow = "auto";
+         }
+         window.addEventListener("click",  function(event) {
+           if (event.target == modal4) {
+             modal4.style.display = "none";
+             document.getElementById('htmls').style.overflow = "auto";
+           }
+        })
+
 });
