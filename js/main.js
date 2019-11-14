4af0bab2b54c51db758f58219f7cc6fbf833a4ac
PetarGajevic <gajevicp0@gmail.com>
2019-11-14 11:41:11 +0100

    slajder

--- js/main.js
+++ js/main.js
@@ -29,6 +29,19 @@ $(document).ready(function(){
     
     })
 
+
+
+    var swiper = new Swiper('.swiper-container', {
+      slidesPerView: 3,
+      spaceBetween: 30,
+      freeMode: true,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+    });
+
+
     // Modal
     var modal = document.getElementById("myModal");
     var btn = document.getElementById("moreText");
