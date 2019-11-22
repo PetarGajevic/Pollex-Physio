9845f0482e374f5fe54d64a2f44e59a7949ede01
PetarGajevic <gajevicp0@gmail.com>
2019-11-22 15:02:05 +0100

    contact form

--- sendmail.php
+++ sendmail.php
@@ -8,7 +8,7 @@ $name = trim($_POST["name"]);
 $email = trim($_POST["email"]);
 $message = trim($_POST["message"]);
 
-print_r($_POST);
+
 
 require_once "PHPMailer/PHPMailer.php";
 require_once "PHPMailer/SMTP.php";
@@ -19,19 +19,19 @@ require 'vendor/autoload.php';
 
 $mail = new PHPMailer(true);
 
-try{
-print_r($mail);
-$mail->SMTPDebug = SMTP::DEBUG_SERVER;   
+
+
+
 $mail->isSMTP();
 $mail->Host = 'n3plcpnl0239.prod.ams3.secureserver.net';
 $mail->SMTPAuth = true;
 $mail->Username = "info@pollexphysio.me";
 $mail->Password = "bnquofzt6gji";
-$mail->port = 465;
-$mail->SMTPSecure = "ssl";
+$mail->Port = 587;
+$mail->SMTPSecure = "tls";
 
 $mail->isHTML(true);
-$mail->setFrom($email, $name);
+$mail->setFrom($email);
 $mail->AddAddress("info@pollexphysio.me");
 $mail->Body = $message;
 
@@ -42,11 +42,7 @@ if($mail->send()){
     $signal = "bad";
     $msg = "Email is not sent";
 }
-} catch (phpmailerException $e){
-    echo $e->errorMessage();
-}catch (Exception $e) {
-    echo $e->getMessage(); //Boring error messages from anything else!
-  }
+
 
 
 /* if($name != null && $email != null && $message != null){
@@ -70,3 +66,4 @@ if($mail->send()){
     echo json_encode($data);
 
 ?>
+
